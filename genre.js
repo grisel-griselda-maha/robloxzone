d1da15f6cba82b4f0df8506ce9c9d2ac05ecc056
Griselda Maha <phytagoories@gmail.com>
2025-11-04 12:12:18 +0700

    Update genre.js to include game links

--- genre.js
+++ genre.js
@@ -5,46 +5,46 @@ const genres = ["lifestyle", "horror", "adventure", "simulator", "sports", "acti
 
 const genreGames = {
   lifestyle: [
-    { name: "Adopt Me!", desc: "Rawat dan pelihara hewan lucu." },
-    { name: "Brookhaven", desc: "Simulasi kehidupan di kota kecil." },
-    { name: "MeepCity", desc: "Sosialisasi dan hias rumahmu." },
-    { name: "Royale High", desc: "Sekolah fantasi untuk para peri." },
-    { name: "Toca City", desc: "Kehidupan bebas di dunia virtual." }
+    { name: "Adopt Me!", link: "https://www.roblox.com/games/920587237/Adopt-Me" },
+    { name: "Brookhaven 🏡RP", link: "https://www.roblox.com/games/4924922222/Brookhaven-RP" },
+    { name: "MeepCity", link: "https://www.roblox.com/games/370731277/MeepCity" },
+    { name: "Royale High", link: "https://www.roblox.com/games/735030788/Royale-High" },
+    { name: "Toca City", link: "#" }
   ],
   horror: [
-    { name: "Doors", desc: "Jelajahi hotel misterius." },
-    { name: "The Mimic", desc: "Legenda Jepang yang menyeramkan." },
-    { name: "Apeirophobia", desc: "Kabur dari dunia liminal." },
-    { name: "Identity Fraud", desc: "Temukan siapa pembunuh di antara kalian." },
-    { name: "Dead Silence", desc: "Adaptasi dari film horor klasik." }
+    { name: "Doors", link: "https://www.roblox.com/games/6516141723/DOORS" },
+    { name: "The Mimic", link: "https://www.roblox.com/games/6243699076/The-Mimic" },
+    { name: "Apeirophobia", link: "https://www.roblox.com/games/10204712304/Apeirophobia" },
+    { name: "Identity Fraud", link: "https://www.roblox.com/games/126689785/Identity-Fraud" },
+    { name: "Dead Silence", link: "https://www.roblox.com/games/1424237/Dead-Silence" }
   ],
   adventure: [
-    { name: "Shindo Life", desc: "Petualangan ninja dan jurus keren." },
-    { name: "Blox Fruits", desc: "Berlayar dan bertarung seperti bajak laut." },
-    { name: "Dragon Adventures", desc: "Pelihara naga dan jelajahi dunia baru." },
-    { name: "Bee Swarm Simulator", desc: "Kumpulkan madu dan lawan musuh." },
-    { name: "Adventure Story", desc: "Petualangan klasik RPG." }
+    { name: "Shindo Life", link: "https://www.roblox.com/games/4616652839/Shindo-Life" },
+    { name: "Blox Fruits", link: "https://www.roblox.com/games/2753915549/Blox-Fruits" },
+    { name: "Dragon Adventures", link: "https://www.roblox.com/games/3475397644/Dragon-Adventures" },
+    { name: "Bee Swarm Simulator", link: "https://www.roblox.com/games/1537690962/Bee-Swarm-Simulator" },
+    { name: "Adventure Story", link: "#" }
   ],
   simulator: [
-    { name: "Pet Simulator X", desc: "Kumpulkan hewan dan koin." },
-    { name: "Strongman Simulator", desc: "Uji kekuatan dan angkat beban." },
-    { name: "Driving Simulator", desc: "Kendarai mobil impianmu." },
-    { name: "Cooking Simulator", desc: "Masak makanan virtual." },
-    { name: "Theme Park Tycoon 2", desc: "Bangun taman hiburan sendiri." }
+    { name: "Pet Simulator X", link: "https://www.roblox.com/games/6284583030/Pet-Simulator-X" },
+    { name: "Strongman Simulator", link: "https://www.roblox.com/games/6766156863/Strongman-Simulator" },
+    { name: "Driving Simulator", link: "https://www.roblox.com/games/8304191830/Driving-Simulator" },
+    { name: "Cooking Simulator", link: "#" },
+    { name: "Theme Park Tycoon 2", link: "https://www.roblox.com/games/69184822/Theme-Park-Tycoon-2" }
   ],
   sports: [
-    { name: "Goal Kick Simulator", desc: "Latih tendanganmu!" },
-    { name: "Basketball Legends", desc: "Main basket seru bareng teman." },
-    { name: "Soccer Stars", desc: "Tanding sepak bola epik." },
-    { name: "Ultimate Football", desc: "Rasakan pengalaman pertandingan." },
-    { name: "Skate Park", desc: "Tunjukkan trik skateboardmu." }
+    { name: "Goal Kick Simulator", link: "https://www.roblox.com/games/9281034295/Goal-Kick-Simulator" },
+    { name: "Basketball Legends", link: "#" },
+    { name: "Soccer Stars", link: "#" },
+    { name: "Ultimate Football", link: "#" },
+    { name: "Skate Park", link: "https://www.roblox.com/games/47324/Skate-Park" }
   ],
   action: [
-    { name: "BedWars", desc: "Hancurkan markas musuh!" },
-    { name: "Arsenal", desc: "Aksi tembak cepat." },
-    { name: "Murder Mystery 2", desc: "Cari tahu siapa pembunuhnya." },
-    { name: "Tower Defense Simulator", desc: "Pertahankan markasmu." },
-    { name: "Phantom Forces", desc: "FPS realistis di Roblox." }
+    { name: "BedWars", link: "https://www.roblox.com/games/6872265039/BedWars" },
+    { name: "Arsenal", link: "https://www.roblox.com/games/286090429/Arsenal" },
+    { name: "Murder Mystery 2", link: "https://www.roblox.com/games/142823291/Murder-Mystery-2" },
+    { name: "Tower Defense Simulator", link: "https://www.roblox.com/games/3260590327/Tower-Defense-Simulator" },
+    { name: "Phantom Forces", link: "https://www.roblox.com/games/292439477/Phantom-Forces" }
   ]
 }
 
@@ -53,9 +53,11 @@ function displayGenre() {
   const container = document.getElementById("genreList")
   container.innerHTML = ""
   genreGames[currentGenre].forEach(game => {
-    const div = document.createElement("div")
+    const div = document.createElement("a")
+    div.href = game.link
+    div.target = "_blank"
     div.className = "game-card"
-    div.innerHTML = `<h3>${game.name}</h3><p>${game.desc}</p>`
+    div.innerHTML = `<h3>${game.name}</h3>`
     container.appendChild(div)
   })
 }
@@ -70,6 +72,10 @@ function prevGenre() {
   const idx = genres.indexOf(currentGenre)
   currentGenre = genres[(idx - 1 + genres.length) % genres.length]
   displayGenre()
+}
+
+function goBack() {
+  window.location.href = "index.html"
 }
 
 displayGenre()
